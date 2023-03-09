64f100d65502a7f340de5afbee2de0c3c159d053
Alice Vitti Kageyama Ferreira <alice220049@ilum.cnpem.br>
2023-03-09 17:34:53 -0300

    Move as funções do experimento A.01 para funcoes.py

--- AlgoritmosGeneticos/funcoes.py
+++ AlgoritmosGeneticos/funcoes.py
@@ -1,3 +1,37 @@
+# Imports:
+
+import random
+
+# Funções:
+
+def gene_cb():
+    ''' Esta função gera um gene válido para o problema das caixas binárias.
+    
+    Return:
+        Um valor 0 ou 1.
+    '''
+    valoresPossíveis = [0, 1]
+    gene = random.choice(valoresPossíveis)
+
+    return gene
+
+def indivíduo_cb(n):
+    ''' Esta função gera um indivíduo para o problema das caixas binárias.
+    
+    Args:
+        n: número de genes de cada indivíduo.
+
+    Return:
+        Uma lista com n genes.
+    '''
+    indivíduo = []
+    
+    for i in range(n):
+        gene = gene_cb()
+        indivíduo.append(gene)
+
+    return indivíduo
+
 def funçãoObjetivo_cb(indivíduo):
     ''' Esta função computa a função objetivo no problema das caixas binárias.
     
