0165935ee39c322222bb3fd551801a8eaadf6be2
Alice Vitti Kageyama Ferreira <alice220049@ilum.cnpem.br>
2023-03-09 17:27:27 -0300

    Adiciona o experimento A.02 resolvido.

--- AlgoritmosGeneticos/funcoes.py
+++ AlgoritmosGeneticos/funcoes.py
@@ -1,1 +1,12 @@
- 
+def funçãoObjetivo_cb(indivíduo):
+    ''' Esta função computa a função objetivo no problema das caixas binárias.
+    
+    Args:
+        indivíduo: lista contendo os n genes.
+
+    Return:
+        A soma dos genes de um indivíduo.
+    '''
+    soma = sum(indivíduo)
+    
+    return soma
